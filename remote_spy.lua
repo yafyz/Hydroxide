26733a67d9621a2cd97652a20b0bce6e187941be
fyz <dvorak.joky@gmail.com>
2020-01-18 16:45:40 +0100

    more attempts

--- remote_spy.lua
+++ remote_spy.lua
@@ -87,6 +87,8 @@ local make_params = function(remote, parameters)
 
     if nofpar == 0 then
         params.Size = UDim2.new(0, 0, 0, 16)
+    else 
+        print(nofpar)
     end
 
     params.MouseButton2Click:Connect(function()
